fc0e884a9ae5192bd2366925398ecd332d881b1b
saniya mehra <mehrasaniya868@gmail.com>
2026-07-06 22:22:48 +0530

    Add featured products section with reusable product cards

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-
+import ProductCard from './ProductCard'
 const ProductSection = () => {
   return (
-    <div className='product-section'>
-
-    </div>
+    <section className="py-5">
+      <div className="container">
+        <h2 className="text-center fw-bold mb-5">
+            Featured Products
+        </h2>
+        <div className="row g-4">
+          <div className="col-md-3">
+            <ProductCard />
+          </div>
+          <div className="col-md-3">
+            <ProductCard />
+          </div>
+          <div className="col-md-3">
+            <ProductCard />
+          </div>
+          <div className="col-md-3">
+            <ProductCard />
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import CategorySection from "../components/CategorySection";
 import HeroSection from "../components/HeroSection";
+import ProductSection from "../components/ProductSection";
 const Home = () => {
   return (
    <>
    <HeroSection/>
    <CategorySection/>
-
+   <ProductSection/>
    <h1 className="text-center my-5">Home Page</h1>
    </>
   )
