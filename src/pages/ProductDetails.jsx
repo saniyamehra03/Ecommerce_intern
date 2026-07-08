0775e2e49cc9a1939ba83bdfc5716a44573087fd
saniya mehra <mehrasaniya868@gmail.com>
2026-07-08 13:19:31 +0530

    Implement dynamic product details page using useParams and find

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import{useParams} from 'react-router-dom'
+import products from '../data/products.json'
+
 const ProductDetails = () => {  
-  const{id}=useParams();
+  const{ id } = useParams();
+  const product = products.find(
+  (item)=> item.id === Number(id)
+);
+if(!product){
+  return <h2>Product not found</h2>;
+}
   return (
-    <h1>Product ID: {id}</h1>
+    <h1>{product.title}</h1>
   )
 }
 
