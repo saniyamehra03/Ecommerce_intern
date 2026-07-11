acb09058d2838e6d078a7b35277b72f1ad1465b8
saniya mehra <mehrasaniya868@gmail.com>
2026-07-11 21:05:21 +0530

    Implement cart item display

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -5,7 +5,17 @@ const Cart = ({ cart }) => {
      <div className="container py-5">
      <h2>Shopping Cart</h2>
      {cart.map((item) => (
-    <h4 key={item.id}>{item.title}</h4>
+    <div 
+    key={item.id} className="card mb-3 p-3" >
+      <img
+      src={item.image}
+      alt={item.title}
+      style={{ width:"120px"}}
+      />
+      <h4>{item.title}</h4>
+      <p>{item.rating}</p>
+      <h5>{item.price}</h5>
+      </div>
     ))}
      </div>
   )
