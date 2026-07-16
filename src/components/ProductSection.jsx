a78be536c363c2cafa966b27875ca341d56fea9b
saniya mehra <mehrasaniya868@gmail.com>
2026-07-17 00:53:25 +0530

    show the product in the based of their category

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,14 +1,76 @@
     import ProductCard from './ProductCard'
     import products from '../data/products.js'
+    import { useState } from 'react'
+
     const ProductSection = ({ addToCart }) => {
+      const [selectedCategory ,setSelectedCategory]= useState("All");
+      const filteredProducts = 
+      selectedCategory === "All"
+      ? products:products.filter(
+        (product) => product.category ===selectedCategory
+      );
       return (
         <section className="py-5">
           <div className="container">
             <h2 className="text-center fw-bold mb-5">
                 Featured Products
             </h2>
+            <div className='d-flex justify-content-center flex-wrap gap-2 mb-5'>
+              <button
+              className={`btn ${
+              selectedCategory === "All"
+              ? "btn-dark"
+              : "btn-outline-dark"
+              }`}
+              onClick={() => selectedCategory("All")}
+              >
+                All
+              </button>
+               <button
+                 className={`btn ${
+                 selectedCategory === "Electronics"
+                 ? "btn-dark"
+                 : "btn-outline-dark"
+               }`}
+                onClick={() => setSelectedCategory("Electronics")}
+              >
+               Electronics
+              </button>
+
+              <button
+              className={`btn ${
+              selectedCategory === "Fashion"
+              ? "btn-dark"
+              : "btn-outline-dark"
+              }`}
+              onClick={() => setSelectedCategory("Fashion")}
+            >
+              Fashion
+             </button>
+             <button
+              className={`btn ${
+              selectedCategory === "Accessories"
+              ? "btn-dark"
+              : "btn-outline-dark"
+             }`}
+              onClick={() => setSelectedCategory("Accessories")}
+            >
+             Accessories
+            </button>
+
+            <button
+              className={`btn ${
+              selectedCategory === "Home"
+              ? "btn-dark"
+              : "btn-outline-dark"
+            }`}
+              onClick={() => setSelectedCategory("Home")}
+            >
+             Home
+            </button>
+            </div>
             <div className="row g-5">
-              {products.map((product) => (
+              {filteredProducts.map((product) => (
                 <div className="col-md-3" key={product.id}>
                   <ProductCard
                   id={product.id}
